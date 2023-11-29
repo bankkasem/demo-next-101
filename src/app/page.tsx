df635f7fcbc7674f626e53ab11291ccd3cf8458b
bankkasem <kasemsarn.purisarn@gmail.com>
2023-11-29 21:53:05 +0700

    feat: :sparkles: add page draft

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,17 +2,45 @@ import React from "react";
 
 const Page = () => {
     return (
-        <div>
-            <h1>Demo</h1>
+        <div className="h-full w-full flex p-4 text-[#555d65]">
+            <section
+                aria-label="menu"
+                className="flex-none w-full md:w-2/3 xl:w-3/4 px-[55px] py-[30px]"
+            >
+                <h1 className="text-2xl">Menu</h1>
+            </section>
 
-            <section style={{ color: "blue" }}>
-                <p style={{ color: "red" }}>
-                    Lorem ipsum dolor sit amet consectetur adipisicing elit.
-                    Odio, praesentium. Unde neque sit voluptate, explicabo,
-                    necessitatibus veritatis cum incidunt deserunt, alias modi
-                    obcaecati. Amet suscipit culpa, aliquid tenetur ab
-                    consequatur?
-                </p>
+            <section
+                aria-label="cart"
+                className="flex-none w-full md:w-1/3 xl:w-1/4"
+            >
+                <div className="h-full w-full flex flex-col gap-2">
+                    <section
+                        aria-label="cart-items"
+                        className="bg-white grow rounded-lg px-[25px] py-[35px]"
+                    >
+                        <h1 className="text-2xl">Cart</h1>
+                    </section>
+
+                    <section aria-label="cart-action" className="space-y-2">
+                        <input
+                            type="button"
+                            value="Address"
+                            className="bg-[#01bfff] text-[#d5f2ff] h-[60px] w-full rounded-lg"
+                        />
+
+                        <input
+                            type="button"
+                            value="Checkout"
+                            className="bg-[#4e565e] text-[#babdc2] h-[60px] w-full rounded-lg"
+                        />
+                    </section>
+
+                    <section
+                        aria-label="cart-actions"
+                        className="flex-initial"
+                    ></section>
+                </div>
             </section>
         </div>
     );
